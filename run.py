82918bc22437f73faa05163363983183830329e3
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-11-01 10:38:38 +0100

    add input questions about health

--- run.py
+++ run.py
@@ -9,4 +9,21 @@ def get_user_input():
         else:
             print("Invalid email address. Please try again.")
 
-nationality = input("Enter your nationality: ").capitalize()            
+nationality = input("Enter your nationality: ").capitalize()  
+
+#Height and weight in numbers 
+height = float(input("Enter your height in meters: "))
+weight = float(input("Enter your weight in kilograms: "))
+
+#Health questions
+training_habits = input("Do you have any training habits? (y/n): ").lower()
+steps_per_day = int(input("How many steps do you take on average per day? "))
+sugar_consuption = ("How often do you consume sugar? (daily/weekly/monthly/rarely): ").lower()
+fast_food_consumption = ("How often do you consume fast food? (daily/weekly/monthly/rarely): ").lower()
+diet_balance = input("Do you eat a balanced diet? (yes/no): ").lower()
+water_consumption = input("Do you drink enough water? (yes/no): ").lower()
+sleep_quality = input("How's your sleep quality? (good/average/poor): ").lower()
+
+
+#Calculate BMI
+          
