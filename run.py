c357a53121130428b8b0332df1324680f01c4079
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-11-01 13:28:32 +0100

    set up gspread and access for google sheets

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('healthdata.json')
+SCOPE_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+SHEET = GSPREAD_CLIENT.open("health_calculator")
+
+
+
 def get_user_input():
     name = input("Enter your name: ")
     email = None
